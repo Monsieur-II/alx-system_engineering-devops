79c0a15fac75eb2c023c9e96656814f8f41ceeb1
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-03-06 14:40:19 +0000

    newupdate

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     '''
         returns the number of subscribers for a given subreddit
     '''
-    user = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    user = {'User-Agent': 'EvansAyeh'}
     response = requests.get('https://www.reddit.com/r/{}/about.json'
                             .format(subreddit), headers=user).json()
     try:
